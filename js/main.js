bb22a6fc7bfd2b32c09cfe9b7f03d58fe8ef7ab3
Leva The Boss <lizardesan@gmail.com>
2016-04-17 15:39:20 +0300

    Add js logic and form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-document.getElementById("deleteEmployer").onClick(function {};)
+$(document).ready(function() {
+	
+	//open edit form
+	$("#editEmployer").on("click", function() {
+		$('.addEmployerPopup_overlay').css('display', 'block');
+	});
+
+	//delete worker and tab
+	$(".deleteEmployer").on("click", function() {
+		if (confirm("Вы действительно хотите удалить запись?")) 
+			$(this).parent().remove();
+	});
+});
