bfdb3889ac1d4802505eb6c1063a2cbdb0218b6a
Leva The Boss <lizardesan@gmail.com>
2016-04-23 10:15:42 +0300

    save

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
 	
+	var base = [
+		{userName: "Вася", userEmail: "none@none.no", comment: ""},
+		{userName: "Петя", userEmail: "none@none.no", comment: ""},
+		{userName: "Коля", userEmail: "none@none.no", comment: ""}
+	];
+
+	function reloadUsers() {
+		for (var i = 0; i < base.length; i++) {
+			console.log('<span class="button" >' + base[i].userName);
+		};
+	}
+
+	reloadUsers();
+
 	//open edit form
-	$("#editEmployer").on("click", function() {
-		$('.addEmployerPopup_overlay').css('display', 'block');
+	$("#addEmployer").on("click", function() {
+		var userName, userEmail, comment = "";
+		$('.popup_overlay').css('display', 'block');
+	});
+
+	//close edit form
+	$(".closeWindow, .popup_overlay").on("click", function() {
+		$('.popup_overlay').css('display', 'none');
+	});
+
+	// prevent events from getting pass .window
+	$(".window").click(function(event){
+		event.stopPropagation();
 	});
 
 	//delete worker and tab
-	$(".deleteEmployer").on("click", function() {
+	$(".deleteWorker").on("click", function() {
 		if (confirm("Вы действительно хотите удалить запись?")) 
 			$(this).parent().remove();
 	});
